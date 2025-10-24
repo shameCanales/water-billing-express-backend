c2dffe493e9319c713d225276cede72d05e570af
shameCanales <canalesshame@gmail.com>
2025-10-24 16:13:46 +0800

    added edit connection by id route

--- src/routes/connections.js
+++ src/routes/connections.js
@@ -91,6 +91,44 @@ router.get(
 );
 
 // edit connection by id
+router.patch(
+  "/api/connections/:id",
+  requireAuthAndStaffOrManager,
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updates = req.body;
+
+      const udpatedConnection = await Connection.findByIdAndUpdate(
+        id,
+        updates,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+
+      if (!udpatedConnection) {
+        return res.status(404).json({
+          success: false,
+          message: "Connection no found",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Connection updated successfully",
+        data: udpatedConnection,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Failed to update connection",
+        error: error.message,
+      });
+    }
+  }
+);
 
 // delete connection by id
 
