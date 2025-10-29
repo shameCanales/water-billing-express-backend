3134c68968dd047ef5210eab491f921b5cd606ea
shameCanales <canalesshame@gmail.com>
2025-10-29 16:18:43 +0800

    added session store via connect-mongo

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import router from "./routes/index.js";
 import passport from "passport";
 import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
 import "./strategies/local-strategy.js";
 
 const app = express();
@@ -24,11 +25,15 @@ app.use(cookieParser("sfmwbs"));
 app.use(
   session({
     secret: "devByShame",
-    saveUninitialized: false,
-    resave: false,
+    saveUninitialized: false, // don't save empty sessions
+    resave: false, // don't save unchanged sessions
     cookie: {
       maxAge: 1000 * 60 * 60,
     },
+    store: MongoStore.create({
+      // to persist session
+      client: mongoose.connection.getClient(),
+    }),
   })
 );
 
@@ -51,8 +56,40 @@ app.get("/", (request, response) => {
   console.log(request.session);
   console.log(request.sessionID);
   request.session.visited = true;
+
+  request.sessionStore.get(request.sessionID, (err, sessionData) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    console.log(sessionData);
+
+    response.status(200).json({
+      success: true,
+      session: sessionData,
+    });
+  });
 });
 
 app.listen(port, () => {
   console.log(`Express Server Running on ${port}`);
 });
+
+// save Uninitialized:::::::::::::::::::
+// false (recommended):
+// Don’t store empty sessions — only save when something is added (like req.session.user = {...}).
+// true:
+// Save all sessions, even if they’re brand new and unused (empty).
+// Why false is better:
+// Saves database space and avoids creating useless session documents in MongoDB.
+// Better performance and privacy compliance (e.g., GDPR).
+
+// Resave:::::::::::::::::::::
+// false (recommended):
+// Only save the session if something changed in req.session.
+// true:
+// Re-save the session on every request, even if nothing changed.
+// Why false is better:
+// Prevents unnecessary writes to MongoDB on every HTTP request.
+// Reduces I/O and improves performance.
